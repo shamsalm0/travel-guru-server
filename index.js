8bcbf8364496d31b525c21e400987be46661db6f
Miad <shamsalm0@gmail.com>
2023-08-06 03:19:34 +0600

    Add url with id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 const cors=require('cors');
 const port=process.env.PORT || 8001;
 
-const placesImg=require('./data/travelling_place.json');
+const places=require('./data/travelling_place.json');
 
 app.use(cors())
 
@@ -12,9 +12,15 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/travellingPlaces',(req,res)=>{
-    res.send(placesImg)
+    res.send(places)
 })
 
+app.get('/travellingPlaces/:id',(req,res)=>{
+    const id=req.params.id;
+    const selectedPlaces=places.find(n=>n.id==id);
+    console.log(selectedPlaces,'hi')
+    res.send(selectedPlaces);
+})
 
 app.listen(port,()=>{
     console.log('daily news server runninng ',port)
