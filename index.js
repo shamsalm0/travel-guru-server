be4ab740881e0108562371e662aa75e41b7545af
Miad <shamsalm0@gmail.com>
2023-09-23 00:23:38 +0600

    creating api for hotel booking

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors=require('cors');
 const port=process.env.PORT || 8001;
 
 const places=require('./data/travelling_place.json');
+const hotel=require('./data/place_booking.json')
 
 app.use(cors())
 
@@ -20,6 +21,14 @@ app.get('/travellingPlaces/:id',(req,res)=>{
     const selectedPlaces=places.find(n=>n.id==id);
     console.log(selectedPlaces,'hi')
     res.send(selectedPlaces);
+})
+app.get('/bookingHotel',(req,res)=>{
+    res.send(hotel)
+})
+app.get('/bookingHotel/:id',(req,res)=>{
+    const id=req.params.id;
+    const selectedHotel=hotel.find(n=>n.id==id);
+    res.send(selectedHotel);
 })
 
 app.listen(port,()=>{
